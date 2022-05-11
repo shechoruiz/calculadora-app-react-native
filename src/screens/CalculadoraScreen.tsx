1ab7638cbbf238a10edbfc78525246b601cc942f
Sergio Andrés Ruiz Vélez <sergio.ruiz@carroya.com>
2022-05-10 21:13:09 -0500

    Definición de función calcular

--- src/screens/CalculadoraScreen.tsx
+++ src/screens/CalculadoraScreen.tsx
@@ -97,6 +97,35 @@ const CalculadoraScreen = () => {
     ultimaOperacion.current = Operadores.sumar;
   };
 
+  const calcular = () => {
+    const num1 = Number(numero);
+    const num2 = Number(numeroAnterior);
+
+    if (num1 === 0 && num2 === 0) {
+      return setNumero('0');
+    }
+
+    switch (ultimaOperacion.current) {
+      case Operadores.sumar:
+        setNumero(`${num1 + num2}`);
+        break;
+      case Operadores.restar:
+        setNumero(`${num2 - num1}`);
+        break;
+      case Operadores.multiplicar:
+        setNumero(`${num1 * num2}`);
+        break;
+      case Operadores.dividir:
+        if (num1 !== 0) {
+          setNumero(`${num2 / num1}`);
+        } else {
+          setNumero('No es un número divisible');
+        }
+        break;
+    }
+    setNumeroAnterior('0');
+  };
+
   return (
     <View style={styles.calculadoraContainer}>
       {numeroAnterior !== '0' && (
@@ -133,7 +162,7 @@ const CalculadoraScreen = () => {
       <View style={styles.fila}>
         <BotonCalc texto="0" accion={armarNumero} ancho />
         <BotonCalc texto="." accion={armarNumero} />
-        <BotonCalc texto="=" color="#ff9427" accion={limpiar} />
+        <BotonCalc texto="=" color="#ff9427" accion={calcular} />
       </View>
     </View>
   );

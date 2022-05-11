7374b53d0bb97ad6c32c812da34015d1b07eaca3
Sergio Andrés Ruiz Vélez <sergio.ruiz@carroya.com>
2022-05-10 21:23:24 -0500

    Creación de custom hook: useCalculadora

--- src/screens/CalculadoraScreen.tsx
+++ src/screens/CalculadoraScreen.tsx
@@ -1,130 +1,23 @@
-import React, {useRef, useState} from 'react';
+import React from 'react';
 import {Text, View} from 'react-native';
 import BotonCalc from '../components/BotonCalc';
 import {styles} from '../theme/appTheme';
-
-enum Operadores {
-  sumar,
-  restar,
-  multiplicar,
-  dividir,
-}
+import useCalculadora from '../hooks/useCalculadora';
 
 const CalculadoraScreen = () => {
-  const [numeroAnterior, setNumeroAnterior] = useState('0');
-  const [numero, setNumero] = useState('0');
-  const ultimaOperacion = useRef<Operadores>();
-
-  const limpiar = () => {
-    setNumero('0');
-    setNumeroAnterior('0');
-  };
-
-  const armarNumero = (numeroTexto: string) => {
-    // No aceptar mas de un punto
-    if (numero.includes('.') && numeroTexto === '.') {
-      return;
-    }
-    if (numero.startsWith('0') || numero.startsWith('-0')) {
-      // 1er punto decimal
-      if (numeroTexto === '.') {
-        setNumero(numero + numeroTexto);
-        //Evaluar si es otro cero y ya hay un punto
-      } else if (numeroTexto === '0' && numero.includes('.')) {
-        setNumero(numero + numeroTexto);
-        // Numero diferente de cero y no hay un punto
-      } else if (numeroTexto !== '0' && !numero.includes('.')) {
-        setNumero(numeroTexto);
-        // Numero diferente de cero y hay un punto
-      } else if (numeroTexto !== '0' && numero.includes('.')) {
-        setNumero(numero + numeroTexto);
-      }
-    } else {
-      setNumero(numero + numeroTexto);
-    }
-  };
-
-  const positivoNegativo = () => {
-    if (numero.includes('-')) {
-      setNumero(numero.replace('-', ''));
-    } else {
-      setNumero('-' + numero);
-    }
-  };
-
-  const btnDel = () => {
-    if (!numero.includes('-')) {
-      if (numero.length > 1) {
-        setNumero(numero.substring(0, numero.length - 1));
-      } else if (numero.length === 1) {
-        setNumero('0');
-      }
-    } else if (numero.includes('-')) {
-      if (numero.length > 2) {
-        setNumero(numero.substring(0, numero.length - 1));
-      } else if (numero.length === 2) {
-        setNumero('0');
-      }
-    }
-  };
-
-  const cambiarNumeroPorAnterior = () => {
-    if (numero.endsWith('.')) {
-      setNumeroAnterior(numero.slice(0, -1));
-    } else {
-      setNumeroAnterior(numero);
-    }
-    setNumero('0');
-  };
-
-  const btnDividir = () => {
-    cambiarNumeroPorAnterior();
-    ultimaOperacion.current = Operadores.dividir;
-  };
-
-  const btnMultiplicar = () => {
-    cambiarNumeroPorAnterior();
-    ultimaOperacion.current = Operadores.multiplicar;
-  };
-
-  const btnRestar = () => {
-    cambiarNumeroPorAnterior();
-    ultimaOperacion.current = Operadores.restar;
-  };
-
-  const btnSumar = () => {
-    cambiarNumeroPorAnterior();
-    ultimaOperacion.current = Operadores.sumar;
-  };
-
-  const calcular = () => {
-    const num1 = Number(numero);
-    const num2 = Number(numeroAnterior);
-
-    if (num1 === 0 && num2 === 0) {
-      return setNumero('0');
-    }
-
-    switch (ultimaOperacion.current) {
-      case Operadores.sumar:
-        setNumero(`${num1 + num2}`);
-        break;
-      case Operadores.restar:
-        setNumero(`${num2 - num1}`);
-        break;
-      case Operadores.multiplicar:
-        setNumero(`${num1 * num2}`);
-        break;
-      case Operadores.dividir:
-        if (num1 !== 0) {
-          setNumero(`${num2 / num1}`);
-        } else {
-          setNumero('No es un número divisible');
-        }
-        break;
-    }
-    setNumeroAnterior('0');
-  };
+  const {
+    numero,
+    numeroAnterior,
+    limpiar,
+    armarNumero,
+    positivoNegativo,
+    btnDel,
+    btnDividir,
+    btnMultiplicar,
+    btnRestar,
+    btnSumar,
+    calcular,
+  } = useCalculadora();
 
   return (
     <View style={styles.calculadoraContainer}>
